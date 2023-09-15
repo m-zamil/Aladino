897b84271e478217464e4fbad2c0ba9ca971b02c
Zamil <muzamil-javascriptdev@outlook.com>
2023-09-15 17:51:07 +0500

    V4 few sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,20 +16,3 @@ function closeMenu() {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }
-
-// Accordion
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
